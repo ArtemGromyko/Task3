519f8b7f0e44211fe698fe448428289e1a6e9067
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-09-15 05:16:16 +0300

    Добавьте файлы проекта.

---
+++ Task3/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Task3
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public Employee()
+        {
+            Name = "";
+            Salary = 0;
+        }
+        public Employee(string n, int s)
+        {
+            Name = n;
+            Salary = s;
+        }
+        public static void MakeEmployeeList(List<Employee> l, int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("enter salary: ");
+                int salary = Convert.ToInt32(Console.ReadLine());
+                l.Add(new Employee(name, salary));
+            }
+        }
+        public static void ShowEmployeeList(List<Employee> l)
+        {
+            Console.WriteLine("count = " +l.Count);
+            for (int i = 0; i < l.Count; i++)
+                Console.WriteLine(i+" : name: "+l[i].Name+" | salary: "+l[i].Salary);
+            Console.WriteLine();
+        }
+    }
+
+    class NameCompare : IComparer<Employee>
+    {
+       public int Compare(Employee e1, Employee e2)
+       {
+            return String.Compare(e1.Name, e2.Name);    
+       }
+    }
+
+    class SalaryComapare : IComparer<Employee>
+    {
+        public int Compare(Employee e1, Employee e2)
+        {
+            if (e1.Salary > e2.Salary)
+                return 1;
+            else if (e1.Salary < e2.Salary)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> l = new List<Employee>(3);
+            Employee.MakeEmployeeList(l, 3);
+            Employee.ShowEmployeeList(l);
+
+            l.Sort(new NameCompare());
+            Employee.ShowEmployeeList(l);
+
+            l.Sort(new SalaryComapare());
+            Employee.ShowEmployeeList(l);
+        }
+    }
+}
